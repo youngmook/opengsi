d41a5068d70bf53d94f19137dad5fb22359d0151
youngmook <youngmook@home-virtualbox>
2014-02-04 21:56:20 +0900

    Added : "opengsi/comsci/config.h"

---
+++ opengsi/comsci/config.h
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * < config.h >
+ *
+ * Library  : OPEN GENERAL SCIENTIFIC INTERFACES(GSI)
+ * E-mail   : help@opengsi.org
+ * Homepage : http://WWW.OPENGSI.ORG
+ * Project  : http://sourceforge.net/projects/open-gsi
+ *
+ ******************************************************************************/
+
+#ifndef __OPEN_GSI_CONFIG_H_
+#define __OPEN_GSI_CONFIG_H_
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+/*
+ # if(!defined(OPENGSI_USE_QT) && !defined(OPENGSI_DO_NOT_USE_QT))
+ #  define OPENGSI_USE_QT 1
+ # endif
+// */
+
+/*
+ # ifndef  OPENGSI_USE_BOOST
+ #  define OPENGSI_USE_BOOST 1
+ # endif
+// */
+
+/*
+ # ifndef  OPENGSI_HAS_NO_ROOT_GSI_OBJECTS
+ #  define OPENGSI_HAS_NO_ROOT_GSI_OBJECTS 1
+ # endif
+// */
+
+#endif  /* __OPEN_GSI_CONFIG_H_ */
+
